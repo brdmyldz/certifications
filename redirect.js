7008e423ddda0f35a1e9b0afc4c469abc6f1a591
Berdem <b2yildiz@uwaterloo.ca>
2022-12-26 02:51:30 -0800

    testing script linking

--- redirect.js
+++ redirect.js
@@ -1,5 +1,6 @@
 //This will be the script that will be shared from many certificate HTML files
 
+document.getElementById("test").innerHTML = "testing..."
 let secondsHTML = document.getElementById("seconds");
 let currentSeconds = 5;
 let countdownCall = setInterval(countdown, 1000); 
