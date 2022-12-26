bce26f0499c9530ec445a18d5a887227eb7080a7
Berdem <b2yildiz@uwaterloo.ca>
2022-12-26 03:24:25 -0800

    fixed redirect.js not loading in HTML

--- redirect.js
+++ redirect.js
@@ -1,12 +1,12 @@
 document.getElementById("test").innerHTML = "testing..."
-let secondsHTML = document.getElementById("seconds");
-let currentSeconds = 5;
-let countdownCall = setInterval(countdown, 1000); 
+var secondsHTML = document.getElementById("seconds");
+var currentSeconds = Number(secondsHTML.innerHTML);
+var countdownCall = setInterval(countdown, 1000); 
 
 function countdown() {
     if (currentSeconds == 0){
         clearInterval(countdownCall);
     }
-    secondsHTML.innerHTML = currentSeconds;
     --currentSeconds;
+    secondsHTML.innerHTML = currentSeconds;
 }
