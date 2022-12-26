de073fc61805e3283474d5b1124299752a5bb906
Berdem <b2yildiz@uwaterloo.ca>
2022-12-26 02:41:38 -0800

    added to logic of redirect countdown

--- redirect.js
+++ redirect.js
@@ -1,1 +1,13 @@
 //This will be the script that will be shared from many certificate HTML files
+
+let secondsHTML = document.getElementById("seconds");
+let currentSeconds = 5;
+let countdownCall = setInterval(countdown, 1000); 
+
+function countdown() {
+    if (currentSeconds == 0){
+        clearInterval(countdownCall);
+    }
+    secondsHTML.innerHTML = currentSecond;
+    --currentSeconds;
+}
